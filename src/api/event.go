d9eb9b59380abf072036b46867988d4e4ad9eed5
gua12345 <121714613+gua12345@users.noreply.github.com>
2025-08-13 18:10:50 +0800

    Update event.go

--- src/api/event.go
+++ src/api/event.go
@@ -431,7 +431,9 @@ func (a *EventApi) Post(c *gin.Context) {
 					}
 				}
 			}
-			break
+			case 0:
+				// 默认级别
+				approve = true
 			default:
 				// 未知的 JoinCheckLevel，默认拒绝加入
 				approve = false
@@ -447,7 +449,8 @@ func (a *EventApi) Post(c *gin.Context) {
 			return
 		}
 	}
-
+	resp.EmptyOk(c)
+}
 
 
 func isManager(sender req.Sender) bool {
