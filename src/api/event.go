cfeea6c576335647ab3a798d843f099129a10c19
gua12345 <121714613+gua12345@users.noreply.github.com>
2025-08-14 17:40:28 +0800

    Revert "增加加群逻辑验证"
    This reverts commit acb9f14c02a437c364b2c35d80848f0aaf800c34.

--- src/api/event.go
+++ src/api/event.go
@@ -226,173 +226,111 @@ func (a *EventApi) Post(c *gin.Context) {
 			if approve {
 				match := GroupAnswerReg.FindStringSubmatch(msg.Comment)
 				if len(match) > 1 {
-					name := regChinese.ReplaceAllString(strings.TrimSpace(match[1]), "")
+					name := strings.TrimSpace(match[1])
 					if name == "" {
 						approve = false
 						reason = "未提供id"
 					} else {
-						switch global.GConfig.QQBot.JoinGroupCheckLevel {
-						case 2:
-							err, data := utils.CheckPlayer(url.QueryEscape(name))
-							if err != nil {
+						err, data := utils.CheckPlayer(url.QueryEscape(name))
+
+						if err != nil || data.PID == "" {
+							if global.GConfig.QQBot.EnableRejectJoinRequest {
+								approve = false
+								if err != nil {
+									if err.Error() == cons.PlayerNotFound {
+										reason = "未能确认你提供的id"
+									} else {
+										reason = "其他异常: " + err.Error()
+									}
+								} else {
+									reason = "pid获取失败"
+								}
+							} else {
 								approve = true
+
 								_ = global.GPool.Submit(func() {
 									time.Sleep(1 * time.Second)
 									// 欢迎信息
-									welcomeMessage := fmt.Sprintf("新玩家 %s 的ID异常, 请管理员注意, 原因: %s", name, err.Error())
+									welcomeMessage := global.GConfig.QQBot.WelcomeMsg + "\n\n机器人已自动修改你的昵称为: [" + name + "]"
 									group.SendAtGroupMsg(msg.GroupID, msg.UserID, welcomeMessage)
+
+									global.GLog.Error("utils.CheckPlayer", zap.Error(err))
+									if err.Error() == cons.PlayerNotFound {
+
+										content := " 机器人无法确认你提供的ID: [" + name + "]，请再次检查并修改你的群名片"
+
+										if global.GConfig.QQBot.EnableAutoKickErrorNickname {
+											content += "。超时将被踢出群聊"
+										}
+
+										group.SendAtGroupMsg(msg.GroupID, msg.UserID, content)
+										// 提供了错误id
+										// 第二次检测在6个小时后
+										// 第三次在48个小时后 第三次检测仍然无法确认的话 则踢出
+										err := dbService.AddCardCheck(msg.UserID, msg.GroupID)
+										if err != nil {
+											global.GLog.Error("dbService.AddCardCheck", zap.Error(err))
+										}
+									} else {
+										group.SendAtGroupMsg(msg.GroupID, msg.UserID,
+											" 机器人已自动修改你的昵称为: ["+name+"]")
+										group.SetCard(msg.GroupID, msg.UserID, name)
+
+										private.SendPrivateMsgMultiple(global.GConfig.QQBot.AdminQq,
+											fmt.Sprintf("ID服务异常, 无法确认qq: %d 提供的id: %s", msg.UserID, name))
+									}
 								})
-							} else {
-								if global.GConfig.QQBot.EnableRejectZeroRankJoinRequest {
-									err, baseInfo := utils.GetPlayerBaseInfo(data.PID)
-									if err != nil {
-										approve = false
-										reason = "获取基础信息失败, 请稍后再试"
-									} else {
-										if baseInfo.BasicStats.Rank.Number == 0 {
-											approve = false
-											reason = "游戏内等级为0, 暂不能进群"
-										} else {
-											approve = true
-										}
-									}
-								} else {
-									approve = true
-								}
 
-								if approve {
-									_ = global.GPool.Submit(func() {
-										time.Sleep(1 * time.Second)
-										// id正确
-										welcomeMessage := global.GConfig.QQBot.WelcomeMsg + "\n\n机器人已自动修改你的昵称为: [" + name + "]"
-										if global.GConfig.QQBot.ShowPlayerBaseInfo {
-											err, finalMsg := utils.GetBaseInfoAndStatusByName(&data)
-											if err == nil {
-												welcomeMessage += "\n\n该玩家基础数据如下:\n\n" + finalMsg
-											}
-										}
-										group.SendAtGroupMsg(msg.GroupID, msg.UserID, welcomeMessage)
-
-										group.SetCard(msg.GroupID, msg.UserID, name)
-
-										if global.GConfig.QQBot.BotToBot.EnableQueryBanRecordByBot {
-											global.GLog.Info("发送封禁记录查询信息",
-												zap.Int64("group_id", msg.GroupID),
-												zap.Int64("bot_qq", global.GConfig.QQBot.BotToBot.BotQq),
-												zap.String("player_name", name))
-											// 发送查询封禁记录的指令
-											queryMsg := fmt.Sprintf("/listban %s", name)
-											group.SendAtGroupMsg(msg.GroupID, global.GConfig.QQBot.BotToBot.BotQq, queryMsg)
-										}
-
-										err = dbService.AddBind(msg.UserID, data.Name, data.PID)
-										if err != nil {
-											global.GLog.Error("dbService.AddBind(msg.UserID, data.Name, data.PID)",
-												zap.Error(err))
-										}
-									})
-								}
 							}
-						default: // 默认等级 1
-							err, data := utils.CheckPlayer(url.QueryEscape(name))
-							if err != nil || data.PID == "" {
-								if global.GConfig.QQBot.EnableRejectJoinRequest {
+						} else {
+							if global.GConfig.QQBot.EnableRejectZeroRankJoinRequest {
+								err, baseInfo := utils.GetPlayerBaseInfo(data.PID)
+								if err != nil {
 									approve = false
-									if err != nil {
-										if err.Error() == cons.PlayerNotFound {
-											reason = "未能确认你提供的id"
-										} else {
-											reason = "其他异常: " + err.Error()
-										}
-									} else {
-										reason = "pid获取失败"
-									}
+									reason = "获取基础信息失败, 请稍后再试"
 								} else {
-									approve = true
-
-									_ = global.GPool.Submit(func() {
-										time.Sleep(1 * time.Second)
-										// 欢迎信息
-										welcomeMessage := global.GConfig.QQBot.WelcomeMsg + "\n\n机器人已自动修改你的昵称为: [" + name + "]"
-										group.SendAtGroupMsg(msg.GroupID, msg.UserID, welcomeMessage)
-
-										global.GLog.Error("utils.CheckPlayer", zap.Error(err))
-										if err.Error() == cons.PlayerNotFound {
-
-											content := " 机器人无法确认你提供的ID: [" + name + "]，请再次检查并修改你的群名片"
-
-											if global.GConfig.QQBot.EnableAutoKickErrorNickname {
-												content += "。超时将被踢出群聊"
-											}
-
-											group.SendAtGroupMsg(msg.GroupID, msg.UserID, content)
-											// 提供了错误id
-											// 第二次检测在6个小时后
-											// 第三次在48个小时后 第三次检测仍然无法确认的话 则踢出
-											err := dbService.AddCardCheck(msg.UserID, msg.GroupID)
-											if err != nil {
-												global.GLog.Error("dbService.AddCardCheck", zap.Error(err))
-											}
-										} else {
-											group.SendAtGroupMsg(msg.GroupID, msg.UserID,
-												" 机器人已自动修改你的昵称为: ["+name+"]")
-											group.SetCard(msg.GroupID, msg.UserID, name)
-
-											private.SendPrivateMsgMultiple(global.GConfig.QQBot.AdminQq,
-												fmt.Sprintf("ID服务异常, 无法确认qq: %d 提供的id: %s", msg.UserID, name))
-										}
-									})
+									if baseInfo.BasicStats.Rank.Number == 0 {
+										approve = false
+										reason = "游戏内等级为0, 暂不能进群"
+									} else {
+										approve = true
+									}
 								}
 							} else {
-								if global.GConfig.QQBot.EnableRejectZeroRankJoinRequest {
-									err, baseInfo := utils.GetPlayerBaseInfo(data.PID)
-									if err != nil {
-										approve = false
-										reason = "获取基础信息失败, 请稍后再试"
-									} else {
-										if baseInfo.BasicStats.Rank.Number == 0 {
-											approve = false
-											reason = "游戏内等级为0, 暂不能进群"
-										} else {
-											approve = true
+								approve = true
+							}
+
+							if approve {
+								_ = global.GPool.Submit(func() {
+									time.Sleep(1 * time.Second)
+									// id正确
+									welcomeMessage := global.GConfig.QQBot.WelcomeMsg + "\n\n机器人已自动修改你的昵称为: [" + name + "]"
+									if global.GConfig.QQBot.ShowPlayerBaseInfo {
+										err, finalMsg := utils.GetBaseInfoAndStatusByName(&data)
+										if err == nil {
+											welcomeMessage += "\n\n该玩家基础数据如下:\n\n" + finalMsg
 										}
 									}
-								} else {
-									approve = true
-								}
+									group.SendAtGroupMsg(msg.GroupID, msg.UserID, welcomeMessage)
 
-								if approve {
-									_ = global.GPool.Submit(func() {
-										time.Sleep(1 * time.Second)
-										// id正确
-										welcomeMessage := global.GConfig.QQBot.WelcomeMsg + "\n\n机器人已自动修改你的昵称为: [" + name + "]"
-										if global.GConfig.QQBot.ShowPlayerBaseInfo {
-											err, finalMsg := utils.GetBaseInfoAndStatusByName(&data)
-											if err == nil {
-												welcomeMessage += "\n\n该玩家基础数据如下:\n\n" + finalMsg
-											}
-										}
-										group.SendAtGroupMsg(msg.GroupID, msg.UserID, welcomeMessage)
+									group.SetCard(msg.GroupID, msg.UserID, name)
 
-										group.SetCard(msg.GroupID, msg.UserID, name)
+									if global.GConfig.QQBot.BotToBot.EnableQueryBanRecordByBot {
+										global.GLog.Info("发送封禁记录查询信息",
+											zap.Int64("group_id", msg.GroupID),
+											zap.Int64("bot_qq", global.GConfig.QQBot.BotToBot.BotQq),
+											zap.String("player_name", name))
+										// 发送查询封禁记录的指令
+										queryMsg := fmt.Sprintf("/listban %s", name)
+										group.SendAtGroupMsg(msg.GroupID, global.GConfig.QQBot.BotToBot.BotQq, queryMsg)
+									}
 
-										if global.GConfig.QQBot.BotToBot.EnableQueryBanRecordByBot {
-											global.GLog.Info("发送封禁记录查询信息",
-												zap.Int64("group_id", msg.GroupID),
-												zap.Int64("bot_qq", global.GConfig.QQBot.BotToBot.BotQq),
-												zap.String("player_name", name))
-											// 发送查询封禁记录的指令
-											queryMsg := fmt.Sprintf("/listban %s", name)
-											group.SendAtGroupMsg(msg.GroupID, global.GConfig.QQBot.BotToBot.BotQq, queryMsg)
-										}
-
-										err = dbService.AddBind(msg.UserID, data.Name, data.PID)
-										if err != nil {
-											global.GLog.Error("dbService.AddBind(msg.UserID, data.Name, data.PID)",
-												zap.Error(err))
-										}
-									})
-								}
+									err = dbService.AddBind(msg.UserID, data.Name, data.PID)
+									if err != nil {
+										global.GLog.Error("dbService.AddBind(msg.UserID, data.Name, data.PID)",
+											zap.Error(err))
+									}
+								})
 							}
 						}
 					}
--- src/common/config/qqbot.go
+++ src/common/config/qqbot.go
@@ -21,7 +21,6 @@ type QQBot struct {
 	EnableRejectJoinRequest         bool                   `mapstructure:"enable-reject-join-request" yaml:"enable-reject-join-request"`
 	EnableRejectZeroRankJoinRequest bool                   `mapstructure:"enable-reject-zero-rank-join-request" yaml:"enable-reject-zero-rank-join-request"`
 	EnableRejectFullGroupJoinRequest bool                   `mapstructure:"enable-reject-full-group-join-request" yaml:"enable-reject-full-group-join-request"`
-	JoinGroupCheckLevel              int                    `mapstructure:"join-group-check-level" json:"join-group-check-level"`
 	// 私有属性
 	activeGroupMap map[int64]bool
 	adminQqMap     map[int64]bool
